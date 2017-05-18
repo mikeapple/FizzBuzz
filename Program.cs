a1ea9c00ba1f9026efba1d334866e431a694af0b
mikeapple <mikeapple@gmail.com>
2017-05-18 10:20:48 -0500

    Initial check-in of module FizBuzz

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FizBuzz
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).ToList().ForEach(e => Console.WriteLine($"{e}: {(e % 3 == 0 ? "Fizz " : string.Empty)}{(e % 5 == 0 ? "Buzz" : string.Empty)}".TrimEnd()));
+            Console.ReadKey();
+        }
+    }
+}
